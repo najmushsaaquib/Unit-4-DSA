948ed8cebd1bb44fbd41b65d1813a58698935a69
najmushsaaquib <saaquibnajmush@gmail.com>
2022-04-07 00:02:50 +0530

    initial binary search

---
+++ Binary Search - April 6th/binary.js
@@ -0,0 +1,22 @@
+// search if the key is present in the arr or not
+
+let arr = [10,20,21,25,30,40,50,60,70]
+let key = 32;
+
+console.log(searchKey(arr,key) )
+
+function searchKey(arr,key) {
+
+    let low = 0;
+    let high = arr.length-1;
+
+    while (low <= high) {
+        let mid = low + (high-low)/2
+
+        if(key == arr[mid]) return mid;
+        else if(key < arr[mid]) high = mid - 1;
+        else low = mid+1
+    }
+
+    return -1;
+}
