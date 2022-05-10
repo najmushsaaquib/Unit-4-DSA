543fda091751ed5b516ffa432af6ee1297de34fa
najmushsaaquib <saaquibnajmush@gmail.com>
2022-05-10 23:57:59 +0530

    break

--- Binary Search - April 6th/eval.js
+++ Binary Search - April 6th/eval.js
@@ -6,3 +6,31 @@ for (let i = 0; i < 18; i++) {
   }
   count++;
 }
+
+
+
+function runProgram(input=''){
+  
+    
+}
+
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
